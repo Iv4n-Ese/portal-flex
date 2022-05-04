0dc12a59e23a3b35cd374af86bc17871257e3099
Iv4n-Ese <su4rez@outlook.com>
2022-05-04 18:03:37 -0500

    portal-flex
    portal en css con flex

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.querySelector(".menu-btn").addEventListener('click', () =>{
+    document.querySelector(".nav-menu").classList.toggle('show')
+});
+
+ScrollReveal().reveal('.showcase', {delay: 200});
+ScrollReveal().reveal('.news-cards', {delay: 400});
+ScrollReveal().reveal('.cards-banner-one', {delay: 450});
+ScrollReveal().reveal('.cards-banner-two', {delay: 450});
+ScrollReveal().reveal('.social', {delay: 500});
+ScrollReveal().reveal('.footer-links', {delay: 480});
